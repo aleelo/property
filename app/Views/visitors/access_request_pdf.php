a6c0410b88bcc7c1638b71fb158c727fae685193
Alihaile <Alihaile2020@gmail.com>
2023-12-28 20:08:42 +0300

    title

--- app/Views/visitors/access_request_pdf.php
+++ app/Views/visitors/access_request_pdf.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <title>Ogolaanshaha soo gelista</title>
         <?php 
         
     $domain = $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
