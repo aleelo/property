20649f3437cdf79893cbf2165fd5412342d63c78
Alihaile <Alihaile2020@gmail.com>
2024-01-24 14:32:28 +0300

    2x

--- app/Views/leaves/leave_qr_code_return.php
+++ app/Views/leaves/leave_qr_code_return.php
@@ -5,7 +5,7 @@
         <style>
             table.dataTable.display tbody th, table.dataTable.display tbody td {
                 border-top: 1px solid #f2f2f2;
-                padding-left: 20px !important;
+                padding-left: 40px !important;
                 background-color: #fcab73 !important;
                 color: white;
             }
@@ -23,7 +23,7 @@
 
                 table.dataTable.display tbody th, table.dataTable.display tbody td {
                     border-top: 1px solid #f2f2f2;
-                    padding-left: 20px !important;
+                    padding-left: 40px !important;
                     background-color: #fcab73 !important;
                     color: white;
                 }
