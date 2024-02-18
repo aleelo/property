a5208e58f21f71983f83986535c87add6dbf230b
Alihaile <Alihaile2020@gmail.com>
2024-02-18 11:28:19 +0300

    pending list issue

--- app/Views/leaves/pending_approval.php
+++ app/Views/leaves/pending_approval.php
@@ -8,6 +8,7 @@
         $("#pending-approval-table").appTable({
             source: '<?php echo_uri("leaves/pending_approval_list_data") ?>',
             columns: [
+                {title: '<?php echo 'ID' ?>', "class": "w10p"},
                 {title: '<?php echo app_lang("applicant") ?>', "class": "w20p"},
                 {title: '<?php echo app_lang("leave_type") ?>'},
                 {title: '<?php echo app_lang("date") ?>', "class": "w20p"},
