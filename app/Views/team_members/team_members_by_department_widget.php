5c880d26ed975ce24a438eceff9da69eb911bf9e
alihaile <alihaile2020@gmail.com>
2024-07-26 15:57:07 +0300

    cols in widget

--- app/Views/team_members/team_members_by_department_widget.php
+++ app/Views/team_members/team_members_by_department_widget.php
@@ -4,23 +4,31 @@
     </div>
     <div class="row rounded-bottom pt-2 pb-3">
         <?php
-        $i = 0;
+        $i = 1;
          foreach ($departments as $k=>$v){ 
-            if($i == 8 || $k == 0){?>                
+            if($i%2 == 0 ){?>                
                 <div class="col-sm-6  b-r-2">
-            <?php }?>
-
+           
                     <div class="pt-2  text-center">
                         <div class="">
-                            <div class="color-tag border-circle me-3 wh10" style="background-color: #e78000;"></div><?php echo $v->department; ?>                        
-                            <span class="strong float-end"><?php echo $v->count; ?></span>
+                            <div class="color-tag border-circle me-3 wh10" style="background-color: #e78000;"></div><?php echo $v?->department; ?>                        
+                            <span class="strong float-end"><?php echo $v?->count; ?></span>
+                        </div>
+                    </div>
+  
+                </div>
+            <?php }else{?>
+                <div class="col-sm-6  b-r-2">
+           
+                    <div class="pt-2  text-center">
+                        <div class="">
+                            <div class="color-tag border-circle me-3 wh10" style="background-color: #e78000;"></div><?php echo $v?->department; ?>                        
+                            <span class="strong float-end"><?php echo $v?->count; ?></span>
                         </div>
                     </div>
 
-                <?php
-                    if($i == 8){?>      
                 </div>
-            <?php $i = 0; }?>
+            <?php }?>
              
         <?php $i++;}?>
     </div>
